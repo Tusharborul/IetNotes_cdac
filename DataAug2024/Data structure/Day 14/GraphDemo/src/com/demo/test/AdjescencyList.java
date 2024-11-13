293334704984d8d0abe8a80ba1bbe9d969052139
TusharBorul <borultushar11@gmail.com>
2024-11-13 18:51:21 +0530

    day13-14 added

---
+++ DataAug2024/Data structure/Day 14/GraphDemo/src/com/demo/test/AdjescencyList.java
@@ -0,0 +1,36 @@
+package com.demo.test;
+
+import java.util.Scanner;
+
+import com.demo.lists.Graph;
+
+public class AdjescencyList {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("How many vertices?");
+		int v=sc.nextInt();		
+		Graph g=new Graph(v);
+		StoreGraph(g,v);
+		g.printGraph();
+		g.DFSTraversal(0);
+		g.BFSTraversal(0);
+
+	}
+	private static void StoreGraph(Graph g,int v) {
+		Scanner sc =new Scanner(System.in);
+		for(int i=0;i<v;i++) {
+			for(int j=0;j<v;j++) {
+				System.out.println("Is there a edge between "+i+"----"+j);
+				int val=sc.nextInt();
+				if(val==1) {
+					g.addEdge(i, j);
+				}
+				
+			}
+		}
+		
+	}
+
+
+}
