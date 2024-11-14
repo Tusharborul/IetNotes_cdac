91d8c4832d690d3634ed3c5d90604cea47c00895
TusharBorul <borultushar11@gmail.com>
2024-11-14 19:23:01 +0530

    advance java day1 added

---
+++ class work/Advance java/Advance Java/Day_1/JDBCDEMO/src/com/demo/test/TestCallableStatement.java
@@ -0,0 +1,33 @@
+package com.demo.test;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestCallableStatement {
+
+	public static void main(String[] args) {
+		try {
+			DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+			
+			DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+			String url="jdbc:mysql://192.168.10.127:3306/dac20?useSSL=false";
+			Connection conn=DriverManager.getConnection(url,"dac20","welcome");
+			CallableStatement cst=conn.prepareCall("call getcnt(?,?)"); 
+			cst.setInt(1, 10);
+			cst.registerOutParameter(2,java.sql.Types.INTEGER );
+			cst.execute();
+			
+			int cnt=cst.getInt(2);
+			System.out.println("Count : "+cnt);
+			
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
---
+++ class work/Advance java/Advance Java/Day_1/JDBCDEMO/src/com/demo/test/TestConnection.java
@@ -0,0 +1,27 @@
+package com.demo.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestConnection {
+
+	public static void main(String[] args) {
+		try {
+			DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+			String url="jdbc:mysql://192.168.10.127:3306/dac20?useSSL=false";
+			Connection conn=DriverManager.getConnection(url,"dac20","welcome");
+			if(conn!=null) {
+				System.out.println("connection done");
+			}else {
+				System.out.println("error occured");
+			}
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+		
+        
+	}
+
+}
